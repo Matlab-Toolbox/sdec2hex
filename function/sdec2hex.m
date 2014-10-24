79986d99c6929c040c0963c97715b1830fe69c9a
Morgan Prior <morgan@amaras-tech.co.uk>
2014-10-24 11:25:41 +0100

    Improved error message when non-integers are input. Other wise an error message about non-negative numbers was also present which is not true for this function.

--- function/sdec2hex.m
+++ function/sdec2hex.m
@@ -2,6 +2,12 @@ function value = sdec2hex(x, wordlength, mode)
 %% SDEC2HEX Signed integer decimal to hexadecimal conversion
 % Currently only Twos complement supported
 
+  %% Input Validation (Integer Check)
+  % double any allows input 2D arrays to be collapsed to a single boolean
+  if ~isreal(x) || any(any(x ~= fix(x)))
+    error('sdec2hex : First argument must contain integers.');
+  end
+
     max_value = 2^(wordlength-1) - 1;
     min_value = -2^(wordlength-1);
     hex_len   = ceil(wordlength/4);
@@ -18,11 +24,12 @@ function value = sdec2hex(x, wordlength, mode)
           error([num2str(cell_value) ' is below min value (', num2str(min_value),') for signed ', num2str(wordlength),' bit wordlength'])
         end
 
+        
         if cell_value < 0
           twos_comp = 2^wordlength + cell_value;
           value(i,j) = {dec2hex(twos_comp,  hex_len)};
         else
-          value(i,j) = {dec2hex(cell_value, hex_len)};
+          value(i,j) = {dec2hex(floor(cell_value), hex_len)};
         end
       end
     end
